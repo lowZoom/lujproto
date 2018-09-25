6929538b7676e0cbba533e53254675b2333d58fe
luj <david_lu_st@163.com>
2018-09-25 21:07:36 +0800

    增加git仓库相关文件

---
+++ example-build/src/main/groovy/build/git/AllModuleCloner.groovy
@@ -0,0 +1,12 @@
+package build.git
+
+import core.AutoCtor
+
+@AutoCtor
+class AllModuleCloner {
+
+
+
+
+  String _buildPath
+}
---
+++ example-build/src/main/groovy/build/git/GitRootFinder.groovy
@@ -0,0 +1,29 @@
+package build.git
+
+import core.AutoCtor
+
+@AutoCtor
+class GitRootFinder {
+
+  File find() {
+    def fileCursor = new File(_startPath)
+
+    while (fileCursor) {
+      if (isGitRoot(fileCursor)) {
+        return fileCursor
+      }
+
+      fileCursor = fileCursor.parentFile
+    }
+
+    return null
+  }
+
+  private boolean isGitRoot(File file) {
+    return file.listFiles()
+        .findAll { it.directory }
+        .findAll { it.name == '.git' }
+  }
+
+  String _startPath
+}
