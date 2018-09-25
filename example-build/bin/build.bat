92eb8473b35b5dacd4eb8ddba32db9fbf0c5e6a0
luj <david_lu_st@163.com>
2018-09-25 17:34:58 +0800

    完善构建启动脚本

--- example-build/bin/build.bat
+++ example-build/bin/build.bat
@@ -1,18 +1,31 @@
-CD %~dp0
-CD env
+@REM
+@ECHO OFF
 
-FOR /D %%G IN (unzip-*) DO SET UNZIP_EXE=%%G\unzip.exe
-%UNZIP_EXE% wget-*.zip
+SET BUILD=%~dp0
+SET BUILD_ENV=%BUILD%env
+SET ENV_TEMP=%BUILD_ENV%\temp
 
-FOR /D %%G IN (wget-*) DO SET WGET_EXE=%%G\wget.exe
-SET GROOVY_ZIP=groovy.zip
-@REM %WGET_EXE% http://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.15.zip -O %GROOVY_ZIP%
+ECHO 搜索unzip...
+FOR /D %%G IN (%BUILD_ENV%\unzip-*) DO SET UNZIP_EXE=%%G\unzip.exe
+ECHO %UNZIP_EXE%
 
-%UNZIP_EXE% %GROOVY_ZIP%
-FOR /D %%G IN (groovy-*) DO SET GROOVY_EXE=%%G\bin\groovy.bat
+ECHO 安装wget...
+%UNZIP_EXE% %BUILD_ENV%\wget-*.zip -d %ENV_TEMP%
 
-SET GROOVY_EXE=%CD%\%GROOVY_EXE%
-CD %~dp0
-CALL %GROOVY_EXE% ..\src\main\groovy\Main.groovy
+FOR /D %%G IN (%ENV_TEMP%\wget-*) DO SET WGET_EXE=%%G\wget.exe
+ECHO %WGET_EXE%
+
+ECHO 下载Groovy运行时...
+SET GROOVY_ZIP=%ENV_TEMP%\groovy.zip
+%WGET_EXE% http://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.15.zip -O %GROOVY_ZIP%
+
+ECHO 安装Groovy运行时...
+%UNZIP_EXE% %GROOVY_ZIP% -d %ENV_TEMP%
+
+FOR /D %%G IN (%ENV_TEMP%\groovy-*) DO SET GROOVY_EXE=%%G\bin\groovy.bat
+ECHO %GROOVY_EXE%
+
+ECHO 运行构建脚本...
+CALL %GROOVY_EXE% %BUILD%..\src\main\groovy\Main.groovy
 
 PAUSE
